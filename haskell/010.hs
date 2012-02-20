2bd568b624cc87a9ca68b0050e0d38906abbabb0
Joshua Poehls <joshua@zduck.com>
2012-02-20 16:54:33 -0600

    Added haskell solution for #10.

---
+++ haskell/010.hs
@@ -0,0 +1,19 @@
+{-
+http://projecteuler.net/problem=10
+
+The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+
+Find the sum of all the primes below two million.
+-}
+
+euler010 :: Int
+euler010 = sum (allPrimesUnder 2000000)
+
+-- Determine if the given number is a prime or not.
+isPrime :: Int -> Bool
+isPrime 2 = True
+isPrime x = null [ y | y <- [3,5..((ceiling . sqrt . fromIntegral) x)], x `mod` y == 0]
+
+-- Gets all primes under the given number.
+allPrimesUnder :: Int -> [Int]
+allPrimesUnder n = takeWhile (< n) (2:[x | x <- [3,5..], isPrime x])
