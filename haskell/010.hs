b4828d4aee3fa2ea3feaf38d85a2ca831aa96a29
Joshua Poehls <joshua@zduck.com>
2012-02-24 21:15:13 -0600

    Added main to #10 for ghc.

--- haskell/010.hs
+++ haskell/010.hs
@@ -9,6 +9,8 @@ Find the sum of all the primes below two million.
 euler010 :: Int
 euler010 = sum (allPrimesUnder 2000000)
 
+main = putStrLn (show euler010)
+
 -- Determine if the given number is a prime or not.
 isPrime :: Int -> Bool
 isPrime 2 = True
